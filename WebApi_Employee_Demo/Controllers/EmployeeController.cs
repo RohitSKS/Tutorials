6d5ddbf9a6e54f203f8b5bdd7cc4fca5d81278cd
Vinod Singh Karki <EE211138@sasken.com>
2019-05-06 11:05:59 +0530

    First Commit

---
+++ WebApi_Employee_Demo/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi_Employee_Demo.Data;
+
+namespace WebApi_Employee_Demo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private int rowCount=5;
+        private List<SampleEmployee> employees = new List<SampleEmployee>();
+        public List<SampleEmployee>  GetAllEmployees(int pageNumber=0)
+        {
+            using (TutorialsEntities1 entities = new TutorialsEntities1())
+            {
+                int startIndex = (pageNumber * rowCount);
+                int endIndex = startIndex + rowCount;
+                if (employees != null)
+                {
+                    employees = entities.SampleEmployees.ToList();
+                }
+                    return employees.Skip(startIndex).Take(endIndex).ToList();
+            }
+        }
+
+        
+    }
+}
